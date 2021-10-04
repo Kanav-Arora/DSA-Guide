5214a9705bdbdff0311848ffa97990bd30a4c9de
Kanav Arora <kanavarora1515@gmail.com>
2021-10-04 11:56:40 +0530

    Minimum Element file added

--- Arrays/Questions/minimum_element.cpp
+++ Arrays/Questions/minimum_element.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        int num;
+        cin>>num;
+        arr[i] = num;
+    }
+
+    int min=arr[0];
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }
+
+    cout<<min<<endl;
+
     return 0;
 }
