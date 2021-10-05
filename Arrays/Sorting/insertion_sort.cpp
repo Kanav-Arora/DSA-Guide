e6cf994d16e05ee55637808db623d4a097d36fbd
Kanav Arora <kanavarora1515@gmail.com>
2021-10-05 10:42:03 +0530

    insertion sort file added

---
+++ Arrays/Sorting/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr[i] = num;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int j = i - 1;
+        while (arr[j] > curr && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = curr;
+    }
+
+    for (int ele : arr)
+    {
+        cout << ele << " ";
+    }
+
+    return 0;
+}
