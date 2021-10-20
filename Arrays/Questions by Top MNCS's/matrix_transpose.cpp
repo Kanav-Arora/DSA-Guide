95fb00f1b8cf2621f8a28686cd9d9a6f372935da
Kanav Arora <kanavarora1515@gmail.com>
2021-10-20 21:48:28 +0530

    matrix_transpose.cpp file modified

--- Arrays/Questions by Top MNCS's/matrix_transpose.cpp
+++ Arrays/Questions by Top MNCS's/matrix_transpose.cpp
@@ -16,11 +16,8 @@ int main()
 
     for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for(int j=i; j<n; j++)
         {
-            if(j<i)
-                continue;
-
             int swap = arr[i][j];
             arr[i][j] = arr[j][i];
             arr[j][i] = swap;
