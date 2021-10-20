d678fb27962228cae8590af4dca6c5bbb807407c
Kanav Arora <kanavarora1515@gmail.com>
2021-10-20 19:50:05 +0530

    spiral_traversal_2d.cpp file modified

--- Arrays/Questions/spiral_traversal_2d.cpp
+++ Arrays/Questions/spiral_traversal_2d.cpp
@@ -2,71 +2,80 @@
                     Spiral Order Matrix Traversal in a 2D Array
 */
 
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
     ifstream file;
     file.open("2d_input.txt");
-    int n,m;
+    int n, m;
     file >> n;
     file >> m;
     int arr[n][m];
     string line;
-    getline(file,line);
-    for(int i=0; i<n; i++)
+    getline(file, line);
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<m; j++)
+        for (int j = 0; j < m; j++)
         {
             file >> arr[i][j];
         }
         getline(file, line);
     }
 
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<m; j++)
+        for (int j = 0; j < m; j++)
         {
-            cout<< arr[i][j]<<" ";
+            cout << arr[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
-    cout<<endl<<endl;
+    cout << endl
+         << endl;
 
     int start_row = 0;
-    int start_column = 0; 
-    int end_row = n-1;
-    int end_column = m-1;
+    int start_column = 0;
+    int end_row = n - 1;
+    int end_column = m - 1;
 
-    while(start_row<=end_row && start_column<=end_column)
+    while (start_row <= end_row && start_column <= end_column)
     {
-        for(int i=start_column; i<=end_column; i++)
+        for (int i = start_column; i <= end_column; i++)
         {
-            cout<<arr[start_row][i]<<" ";
+            cout << arr[start_row][i] << " ";
         }
-        cout<<endl;
+        cout << endl;
         start_row++;
-        for(int i=start_row; i<=end_row; i++)
+        for (int i = start_row; i <= end_row; i++)
         {
-            cout<<arr[i][end_column]<<" ";
+            cout << arr[i][end_column] << " ";
         }
-        cout<<endl;
+        cout << endl;
         end_column--;
-        for(int i=end_column; i>=start_column; i--)
+
+        if(start_row<=end_row)
         {
-            cout<<arr[end_row][i]<<" ";
+        for (int i = end_column; i >= start_column; i--)
+        {
+            cout << arr[end_row][i] << " ";
         }
-        cout<<endl;
+        cout << endl;
+        }
         end_row--;
-        for(int i=end_row; i>=start_row; i--)
+        if (start_column <= end_column)
         {
-            cout<<arr[i][start_column]<<" ";
+
+            for (int i = end_row; i >= start_row; i--)
+            {
+                cout << arr[i][start_column] << " ";
+            }
+            cout << endl;
         }
-        cout<<endl;
         start_column++;
     }
 
