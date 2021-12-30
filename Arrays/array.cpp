e903b24428cf9419806bff8026915c064d7aebf4
Kanav Arora <kanavarora1515@gmail.com>
2021-12-30 17:40:08 +0530

    array.cpp file modified

--- Arrays/array.cpp
+++ Arrays/array.cpp
@@ -13,5 +13,14 @@ int main()
 
     int array[4] = {0,100,200,300};
     cout<<array[3]<<endl;
+
+    int one[5] = {1};
+    int n= 5;
+    for(int i=0; i<n; i++)
+    {
+        cout<<one[i]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
