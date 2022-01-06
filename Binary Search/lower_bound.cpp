74ebd2a2548685cc0be9881e05749a84b6f5f520
Kanav Arora <kanavarora1515@gmail.com>
2022-01-06 23:49:35 +0530

    upper bound and lower bound files added

---
+++ Binary Search/lower_bound.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+
+int lower_bound(vector<int> nums, int target)
+{
+    int start = 0;
+    int end = nums.size()-1;
+    int mid = 0;
+    while(start<end)
+    {
+        mid = start  + (end-start)/2;
+        if(nums[mid]<target)    start = mid+1;
+        else   end = mid;
+    }
+    return start;
+}
+
+int main()
+{
+    vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    nums.push_back(6);
+    nums.push_back(6);
+    nums.push_back(8);
+    nums.push_back(11);
+    cout<<lower_bound(nums,6);
+    return 0;
+}
