0bd0b198c7f84f8ce0bee3cfb7ab182a88903e65
Kanav Arora <kanavarora1515@gmail.com>
2022-06-06 14:52:41 +0530

    first_and_last_occurence.cpp file added

--- Arrays/Searching/Binary Search/Questions/first_and_last_occurence.cpp
+++ Arrays/Searching/Binary Search/Questions/first_and_last_occurence.cpp
@@ -1,8 +1,44 @@
 #include<iostream>
 using namespace std;
 
+pair<int,int> firstLastOccurence(int arr[], int n, int target)
+{
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+
+    while(start<end)
+    {
+        mid = start + (end-start)/2;
+        if(arr[mid]<target)     start = mid+1;
+        else    end = mid;
+    }
+
+    int first = start;
+
+    start = 0;
+    end = n-1;
+    mid = start + (end-start)/2;
+
+    while(start<end)
+    {
+        mid = start + (end-start)/2;
+        if(arr[mid]<=target)     start = mid;
+        else    end = mid-1;
+    }
+
+    int last = start;
+
+    return {first,last};
+}
+
+
 int main()
 {
-    
+    int arr[] = {1,4,5,6,10,10,10,12,14};
+    pair<int,int> p = firstLastOccurence(arr, 9, 10);
+
+    cout<<p.first<<" "<<p.second<<endl;
+
     return 0;
 }
