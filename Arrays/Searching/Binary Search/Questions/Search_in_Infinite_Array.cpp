0030002d77b3b84cc3dea25f34c0f0f67ea2c1bb
Kanav Arora <kanavarora1515@gmail.com>
2022-06-06 19:38:09 +0530

    Search_in_Infinite_Array.cpp file added

---
+++ Arrays/Searching/Binary Search/Questions/Search_in_Infinite_Array.cpp
@@ -0,0 +1,13 @@
+/*
+    Search an element in an infinite sorted array
+
+
+
+    For an infinite array take low = 0  high =1
+
+    apply B.S and increase high = high*2 till key<arr[high] as key is bounded by low and high
+*/
+
+
+
+
