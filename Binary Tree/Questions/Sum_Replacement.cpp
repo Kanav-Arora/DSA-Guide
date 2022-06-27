a56f96e40f086c777182e4bc7eed49b9d8a0816d
Kanav Arora <kanavarora1515@gmail.com>
2022-06-27 21:11:58 +0530

    Sum_Replacement.cpp file added

---
+++ Binary Tree/Questions/Sum_Replacement.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public: 
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val)
+        {
+            data = val;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+void printBT(const std::string& prefix, const Node* node, bool isLeft)
+{
+    if (node != nullptr)
+    {
+        std::cout << prefix;
+        std::cout << (isLeft ? "|--" : "L--");
+        std::cout << node->data << std::endl;
+        printBT(prefix + (isLeft ? "|   " : "    "), node->right, true);
+        printBT(prefix + (isLeft ? "|   " : "    "), node->left, false);
+    }
+}
+
+void printBT(const Node* node)
+{
+    printBT("", node, false);
+}
+
+int SumReplacement(Node* root)
+{
+    if(root==NULL)
+        return 0;
+
+    int lefts = SumReplacement(root->left);
+    int rights = SumReplacement(root->right);
+
+    root->data += lefts + rights;
+
+    return root->data;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    printBT(root);
+
+    SumReplacement(root);
+
+    printBT(root);
+    return 0;
+}
