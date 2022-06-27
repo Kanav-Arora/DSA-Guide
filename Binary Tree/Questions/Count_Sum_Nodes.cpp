fc083d0e591708b6f65d0c237c8d67da69eeb32d
Kanav Arora <kanavarora1515@gmail.com>
2022-06-27 18:19:05 +0530

    Count_Sum_Nodes.cpp file added

---
+++ Binary Tree/Questions/Count_Sum_Nodes.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public: 
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val)
+        {
+            data = val;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+void printBT(const std::string& prefix, const Node* node, bool isLeft)
+{
+    if (node != nullptr)
+    {
+        std::cout << prefix;
+        std::cout << (isLeft ? "|--" : "L--");
+        std::cout << node->data << std::endl;
+        printBT(prefix + (isLeft ? "|   " : "    "), node->right, true);
+        printBT(prefix + (isLeft ? "|   " : "    "), node->left, false);
+    }
+}
+
+void printBT(const Node* node)
+{
+    printBT("", node, false);
+}
+
+
+int CountNodes(Node* node)
+{
+    if(node==NULL)
+        return 0;
+    
+    return CountNodes(node->left) + CountNodes(node->right) + 1;
+}
+
+int SumNode(Node* node)
+{
+    if(node==NULL)
+        return 0;
+    
+    return SumNode(node->left) + SumNode(node->right) + node->data;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout<<CountNodes(root)<<endl;
+    cout<<SumNode(root)<<endl;
+    return 0;
+}
