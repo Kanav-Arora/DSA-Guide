e8a289940a61040b42cca228400ba0a7319d8b8b
Kanav Arora <kanavarora1515@gmail.com>
2022-06-28 14:54:39 +0530

    Right_View_BT.cpp file added

--- Binary Tree/tree.cpp
+++ Binary Tree/tree.cpp
@@ -15,6 +15,23 @@ class Node{
         }
 };
 
+void printBT(const std::string& prefix, const Node* node, bool isLeft)
+{
+    if (node != nullptr)
+    {
+        std::cout << prefix;
+        std::cout << (isLeft ? "|--" : "L--");
+        std::cout << node->data << std::endl;
+        printBT(prefix + (isLeft ? "|   " : "    "), node->right, true);
+        printBT(prefix + (isLeft ? "|   " : "    "), node->left, false);
+    }
+}
+
+void printBT(const Node* node)
+{
+    printBT("", node, false);
+}
+
 int main()
 {
     Node* root = new Node(1);
