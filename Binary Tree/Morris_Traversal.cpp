5a6217490b5e658f39d0e7d11d58df8f6e7c0eb3
Kanav Arora <kanavarora1515@gmail.com>
2022-07-13 16:20:49 +0530

    Morris_Traversal.cpp PostOrder function added

--- Binary Tree/Morris_Traversal.cpp
+++ Binary Tree/Morris_Traversal.cpp
@@ -1,22 +1,24 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include<algorithm>
 using namespace std;
 
-class Node{
-    public: 
-        int data;
-        Node* left;
-        Node* right;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
 
-        Node(int val)
-        {
-            data = val;
-            left = NULL;
-            right = NULL;
-        }
+    Node(int data)
+    {
+        data = data;
+        left = NULL;
+        right = NULL;
+    }
 };
 
-void printBT(const std::string& prefix, const Node* node, bool isLeft)
+void printBT(const std::string &prefix, const Node *node, bool isLeft)
 {
     if (node != nullptr)
     {
@@ -28,18 +30,18 @@ void printBT(const std::string& prefix, const Node* node, bool isLeft)
     }
 }
 
-void printBT(const Node* node)
+void printBT(const Node *node)
 {
     printBT("", node, false);
 }
 
-vector<int> InOrder(Node* root)
+vector<int> InOrder(Node *root)
 {
     vector<int> ans;
-    Node* curr = root;
-    while(curr!=NULL)
+    Node *curr = root;
+    while (curr != NULL)
     {
-        if(curr->left==NULL)
+        if (curr->left == NULL)
         {
             ans.push_back(curr->data);
             curr = curr->right;
@@ -47,36 +49,35 @@ vector<int> InOrder(Node* root)
 
         else
         {
-            Node* temp = curr->left;
-            while(temp->right && temp->right!=curr)
+            Node *temp = curr->left;
+            while (temp->right && temp->right != curr)
                 temp = temp->right;
 
-            if(!temp->right)
+            if (!temp->right)
             {
                 temp->right = curr;
                 curr = curr->left;
             }
-            
+
             else
             {
                 temp->right = NULL;
                 ans.push_back(curr->data);
                 curr = curr->right;
             }
-            
         }
     }
 
     return ans;
 }
 
-vector<int> PreOrder(Node* root)
+vector<int> PreOrder(Node *root)
 {
     vector<int> ans;
-    Node* curr = root;
-    while(curr!=NULL)
+    Node *curr = root;
+    while (curr != NULL)
     {
-        if(curr->left==NULL)
+        if (curr->left == NULL)
         {
             ans.push_back(curr->data);
             curr = curr->right;
@@ -84,32 +85,70 @@ vector<int> PreOrder(Node* root)
 
         else
         {
-            Node* temp = curr->left;
-            while(temp->right && temp->right!=curr)
+            Node *temp = curr->left;
+            while (temp->right && temp->right != curr)
                 temp = temp->right;
 
-            if(!temp->right)
+            if (!temp->right)
             {
                 temp->right = curr;
                 ans.push_back(curr->data);
                 curr = curr->left;
             }
-            
+
             else
             {
                 temp->right = NULL;
                 curr = curr->right;
             }
-            
         }
     }
+
+    return ans;
+}
+
+vector<int> PostOrder(Node *root)
+{
+    vector<int> ans;
+    if (root == NULL)
+        return ans;
+
+    Node *curr = root;
+    while (curr)
+    {
+        if (curr->right == NULL)
+        {
+            ans.push_back(curr->data);
+            curr = curr->left;
+        }
+        else
+        {
+            Node *temp = curr->right;
+            while (temp->left && temp->left != curr)
+                temp = temp->left;
+
+            if (temp->left == NULL)
+            {
+                temp->left = curr;
+                ans.push_back(curr->data);
+                curr = curr->right;
+            }
+            else
+            {
+                temp->left = NULL;
+                curr = curr->left;
+            }
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
 
     return ans;
 }
 
 int main()
 {
-    Node* root = new Node(1);
+    Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     return 0;
