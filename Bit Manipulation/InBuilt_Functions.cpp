5d90c918996de2db36359cfa61c617568d7414f5
Kanav Arora <kanavarora1515@gmail.com>
2023-01-21 13:44:32 +0530

    InBuilt gcc functions on bits

---
+++ Bit Manipulation/InBuilt_Functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// These functions are defined in gcc compiler and they have a best case time complexity of O(1)
+// and an average case time complexity of O(log_2 k) where k is the position of set bit
+
+int main()
+{
+    cout << __builtin_clz(5) << endl; // count no of zero bits at the beginning of a number
+
+    cout << __builtin_ctz(5) << endl; // count no of zero bits at the end of a number
+
+    cout << __builtin_popcount(5) << endl; // number of 1's in bit
+
+    cout << __builtin_parity(5) << endl; // returns 1 if no of 1's in number is odd else 0
+    return 0;
+}
