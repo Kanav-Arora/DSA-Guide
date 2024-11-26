9bd101dc6419c0aa1bfcbd31c3bf9faf305877a2
Kanav Arora <kanavarora1515@gmail.com>
2024-11-26 18:31:26 +0530

    Revised CP Template

--- COMPETITIVE_TEMPLATE.cpp
+++ COMPETITIVE_TEMPLATE.cpp
@@ -1,38 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <limits>
+#include <algorithm> // for sort
+#include <limits>    // for numeric limits
+#include <cmath>     // for mathematical operations
 using namespace std;
 
-#define gc getchar_unlocked;
-#define fo(i, n) for (int i = 0; i < n; i++)
-#define foc(i, n) for (int i = 0; i <= n; i++)
-#define rfo(i, s, e) for (int i = s; i < e; i++)
-#define rfoc(i, s, e) for (int i = s; i <= e; i++)
-#define revfo(i, e) for (int i = e; i >= 0; i--)
-#define ll long long
-#define deb(x) cout << #x << " = " << x << endl;
-#define deb2(x, y) cout << #x << " = " << x << " , " << #y << " = " << y << endl;
-#define pb push_back
-#define mp make_pair
-#define f first
-#define s second
-#define all(x) x.begin(), x.end()
-#define sortall(x) sort(all(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
-#define PI 3.141592653589793238
-#define inf numeric_limits<int>::max()
-#define neginf numeric_limits<int>::min()
+#define fo(i, n) for (int i = 0; i < n; i++)       // 0-based loop
+#define foc(i, n) for (int i = 0; i <= n; i++)     // 0-based loop inclusive
+#define rfo(i, s, e) for (int i = s; i < e; i++)   // Start-to-end exclusive
+#define rfoc(i, s, e) for (int i = s; i <= e; i++) // Start-to-end inclusive
+#define revfo(i, e) for (int i = e; i >= 0; i--)   // Reverse loop
 
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<bool> vb;
-typedef vector<pii> vpii;
+// Data type shortcuts
+#define ll long long
+#define pii pair<int, int>
+#define pl pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vb vector<bool>
+#define vpii vector<pii>
+
+// Utility macros
+#define deb(x) cout << #x << " = " << x << endl;                                  // Debug single variable
+#define deb2(x, y) cout << #x << " = " << x << " , " << #y << " = " << y << endl; // Debug two variables
+#define pb push_back                                                              // Shortcut for push_back
+#define mp make_pair                                                              // Shortcut for make_pair
+#define f first                                                                   // Access the first element of a pair
+#define s second                                                                  // Access the second element of a pair
+#define all(x) x.begin(), x.end()                                                 // Range shortcut for containers
+#define sortall(x) sort(all(x))                                                   // Sort an entire container
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)                  // Traverse a container
+
+// Constants
+#define PI 3.141592653589793238           // Approximate value of π
+#define inf numeric_limits<int>::max()    // Maximum integer value
+#define neginf numeric_limits<int>::min() // Minimum integer value
 
 int max(int v1, int v2)
 {
     return v1 < v2 ? v2 : v1;
+}
+
+// GCD (Greatest Common Divisor) using Euclid's algorithm
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// LCM (Least Common Multiple) based on GCD
+int lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+// Modular exponentiation (for large power computations)
+ll mod_exp(ll base, ll exp, ll mod)
+{
+    ll result = 1;
+    base %= mod;
+    while (exp > 0)
+    {
+        if (exp % 2 == 1)
+            result = (result * base) % mod;
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
 }
 
 int main()
@@ -40,9 +79,5 @@ int main()
     int t = 1;
     cin >> t;
 
-    while (t--)
-    {
-        cout << inf << endl;
-    }
     return 0;
 }
