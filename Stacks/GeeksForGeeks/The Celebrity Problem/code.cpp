7ded6cf114b4b628021695ce3ac17fd561916811
Kanav Arora <kanavarora1515@gmail.com>
2024-11-28 21:46:17 +0530

    GFG The Celebrity Problem added

---
+++ Stacks/GeeksForGeeks/The Celebrity Problem/code.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    int celebrity(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        stack<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            st.push(i);
+        }
+        while (st.size() > 1)
+        {
+            int a = st.top();
+            st.pop();
+            int b = st.top();
+            st.pop();
+            if (mat[a][b] == 1)
+                st.push(b);
+            else
+                st.push(a);
+        }
+        if (st.empty())
+            return -1;
+        int ans = st.top();
+        for (int i = 0; i < n; i++)
+        {
+            if (i != ans)
+            {
+                if (mat[ans][i] == 1 || mat[i][ans] == 0)
+                {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+};
