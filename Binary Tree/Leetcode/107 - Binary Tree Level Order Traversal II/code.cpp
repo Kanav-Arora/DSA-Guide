4a73bd630ac5d62aa4634d94c3b39ec2f7be4fb0
Kanav Arora <kanavarora1515@gmail.com>
2024-11-29 18:02:17 +0530

    LC 107 - Binary Tree Level Order Traversal II added

---
+++ Binary Tree/Leetcode/107 - Binary Tree Level Order Traversal II/code.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode *root)
+    {
+        vector<vector<int>> ans;
+        if (!root)
+            return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int n = q.size();
+            vector<int> temp;
+            while (n--)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                temp.push_back(node->val);
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            ans.push_back(temp);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
