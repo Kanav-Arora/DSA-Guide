e9e272b2bae5a76548dc346d09a8f6e497c82a55
Kanav Arora <kanavarora1515@gmail.com>
2024-12-02 17:43:32 +0530

    LC 236 - Lowest Common Ancestor of a Binary Tree added

---
+++ Binary Tree/Leetcode/236 - Lowest Common Ancestor of a Binary Tree/code.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *caller(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (!root)
+            return NULL;
+        if (root == p || root == q)
+            return root;
+        TreeNode *lt = caller(root->left, p, q);
+        TreeNode *rt = caller(root->right, p, q);
+
+        if (lt && rt)
+            return root;
+        return rt ? rt : lt;
+    }
+
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        return caller(root, p, q);
+    }
+};
